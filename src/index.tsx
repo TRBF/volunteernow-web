2f42e5b0caee8ffb774e81f13a2b89e24d507e82
trbf <trbf.contact@gmail.com>
2025-04-22 18:40:28 +0300

    fix: Remove basename from BrowserRouter to serve at root URL

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <BrowserRouter basename="/volunteernow-web">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </LocalizationProvider>
